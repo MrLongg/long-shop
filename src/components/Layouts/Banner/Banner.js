d20fe0fccf7ce93d2c9d4285ae7cadb87164f6d9
Long <trandohoanglong113@gmail>
2024-01-14 17:52:09 +0700

    Đang làm banner

--- src/components/Layouts/Banner/Banner.js
+++ src/components/Layouts/Banner/Banner.js
@@ -4,7 +4,17 @@ import styles from './Banner.module.scss';
 const cx = classNames.bind(styles);
 
 function Banner() {
-    return <div className={cx('wrapper')}>Banner</div>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('img-container')}>
+                <img
+                    className={cx('img')}
+                    src="https://media.istockphoto.com/id/923079848/photo/online-shopping.jpg?s=1024x1024&w=is&k=20&c=e7TS1ukDwc2Wkyq3lf3QLO8jgsOOsbZDrrruiQwd1aE="
+                    alt=""
+                />
+            </div>
+        </div>
+    );
 }
 
 export default Banner;
